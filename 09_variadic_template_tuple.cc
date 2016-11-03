4ef2d59d6447648a95456d61da0a0ecd92f0fc85
Miguel Péres <foxmpc@gmail.com>
2016-11-03 18:57:01 -0200

    Implement at function (my version of std::get). Not working for tuples with mixes types.

--- 09_variadic_template_tuple.cc
+++ 09_variadic_template_tuple.cc
@@ -7,8 +7,9 @@ struct Tuple {
 	Tuple<Rest...> tail;
 };
 
-// Caso específico deve vir depois do caso genérico, 
-// e com o tipo especificado na declaração
+/* Caso específico deve vir depois do caso genérico, e com o tipo especificado na declaração */
+
+// Caso base: último membro da tupla, não terá cauda
 template <typename Head>
 struct Tuple<Head> {
 	Head head;
@@ -31,17 +32,38 @@ Tuple<First, Rest...> init_tuple(First first, Rest... rest) {
 	return tuple;
 }
 
+template <int i, typename Head>
+Head at(Tuple<Head>& tuple) {
+	if(i == 0)
+		return tuple.head;
+	else
+		throw "Error: tuple size exceeded.";
+}
+
+template <int i, typename Head, typename... Tail>
+Head at(Tuple<Head, Tail...>& tuple) {
+	if(i == 0)
+		return tuple.head;
+	else
+		return at<(i-1)>(tuple.tail);
+}
 
 int main() {
 	using namespace std;
 
 	Tuple<int, string, double> t{ 10, { "abc", { 3.14 } } };
 
-	auto tuple = init_tuple("one", 2, 3.0);
+	auto tuple = init_tuple(1, 2, 3);
 
-	cout << tuple.head << endl;
-	cout << tuple.tail.head << endl;
-	cout << tuple.tail.tail.head << endl;
+	/* Getting values manually */
+	// cout << tuple.head << endl;
+	// cout << tuple.tail.head << endl;
+	// cout << tuple.tail.tail.head << endl;
+
+	/* Using at function */
+	cout << at<0>(tuple) << endl;
+	cout << at<1>(tuple) << endl;
+	cout << at<2>(tuple) << endl;
 
 	return 0;
 }
